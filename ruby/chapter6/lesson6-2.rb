baa9d7aa29061cbc1bf3704b0f8763ac6fbb5363
TakatoTokuda <119557028+tokudatakato@users.noreply.github.com>
2022-12-05 03:01:37 +0000

    [finish]2-7

---
+++ ruby/chapter6/lesson6-2.rb
@@ -0,0 +1,27 @@
+puts "計算をはじめます"
+puts "何回計算を繰り返しますか？"
+
+repeat = gets.to_i
+i = 0
+
+while i != repeat do
+  i += 1
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+  
+  a = gets.to_i
+  b = gets.to_i
+  
+  puts "a=#{a}"
+  puts "b=#{b}"
+  
+  puts "計算結果を出力します"
+  
+  puts "#{a}+#{b}=#{a+b}"
+  puts "#{a}-#{b}=#{a-b}"
+  puts "#{a}*#{b}=#{a*b}"
+  puts "#{a}/#{b}=#{a/b}"
+  
+end
+
+puts "計算を終了します"
