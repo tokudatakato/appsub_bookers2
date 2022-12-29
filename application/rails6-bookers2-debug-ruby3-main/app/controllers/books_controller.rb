db3409b79758760145761647b8b478ecafc83527
TakatoTokuda <119557028+tokudatakato@users.noreply.github.com>
2022-12-29 01:04:26 +0000

    [finish]2

--- application/rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
+++ application/rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :is_matching_login_user, only: [:edit, :update]
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
 
   def new
     @book = Book.new
